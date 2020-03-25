099164f6171d687722bb22a99dcd7ac6ef76a361
salman mithani <salmanmithani@salmans-MacBook-Pro.local>
2020-03-26 04:27:06 +0500

    add feature

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCcqJH3UAO1KjP-22c4P1owABV9NviymUM",
+    authDomain: "fir-rn-6c9fc.firebaseapp.com",
+    databaseURL: "https://fir-rn-6c9fc.firebaseio.com",
+    projectId: "fir-rn-6c9fc",
+    storageBucket: "fir-rn-6c9fc.appspot.com",
+    messagingSenderId: "114374215276",
+    appId: "1:114374215276:web:ba59c711ab6b5acff87a49"
+  };
